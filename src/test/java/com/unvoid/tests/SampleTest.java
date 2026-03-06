32dd902f47c2513848b8c78640b2e10f2ba0c7bf
Pavan_🐯 <pvnwagh332@gmail.com>
2026-03-06 21:21:30 +0530

    Configure ChromeDriver for headless testing
    Updated ChromeDriver initialization to use ChromeOptions for headless execution.

--- src/test/java/com/unvoid/tests/SampleTest.java
+++ src/test/java/com/unvoid/tests/SampleTest.java
@@ -3,13 +3,24 @@ package com.unvoid.tests;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SampleTest {
+
     @Test
     public void openHomePage() {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://localhost:3000"); // replace with your app URL
+
+        // Setup Chrome options for Jenkins
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); 
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.get("http://localhost:3000"); 
         System.out.println("Page title: " + driver.getTitle());
+
         driver.quit();
     }
 }
