1a8f227655c27c1bc5a0347eaf779840c1d7587a
Pavan_🐯 <pvnwagh332@gmail.com>
2026-03-06 23:49:36 +0530

    Update SampleTest.java

--- src/test/java/com/unvoid/tests/SampleTest.java
+++ src/test/java/com/unvoid/tests/SampleTest.java
@@ -4,13 +4,13 @@ import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 public class SampleTest {
 
     @Test
     public void openHomePage() {
 
-        System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
